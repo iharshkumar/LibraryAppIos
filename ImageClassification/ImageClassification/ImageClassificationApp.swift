2c3dbbfd7f556c92288f75cc33f3e38611505668
BATCH01L1-15 <batch01l1-15@Admins-iMac.local>
2025-11-22 16:16:38 +0530

    ClassificationmodelMobileNetV2

---
+++ ImageClassification/ImageClassification/ImageClassificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationApp.swift
+//  ImageClassification
+//
+//  Created by BATCH01L1-15 on 22/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageClassification/ImageClassificationTests/ImageClassificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationTests.swift
+//  ImageClassificationTests
+//
+//  Created by BATCH01L1-15 on 22/11/25.
+//
+
+import Testing
+@testable import ImageClassification
+
+struct ImageClassificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
