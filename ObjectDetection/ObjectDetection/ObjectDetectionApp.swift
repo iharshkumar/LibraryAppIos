9c6277d2f09e57630bd7cde8d861d76033b88a2a
BATCH01L1-15 <batch01l1-15@Admins-iMac.local>
2025-11-22 16:18:53 +0530

    Mobilenet V2

---
+++ ObjectDetection/ObjectDetection/ObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectionApp.swift
+//  ObjectDetection
+//
+//  Created by BATCH01L1-15 on 22/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
